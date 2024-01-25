8745f23d8ae6186e80ad7c65a26eae2321c719dd
Tracy Li <tracy.li@kellogg.northwestern.edu>
2024-01-25 03:34:48 +0000

    Completed the questions

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -8,4 +8,23 @@
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+## long way
+# set the index to 0
+index = 0
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+#start the loop
+loop do
+    if index == tacos.length
+        break
+    end # if the index is outside the range of the array, stop looping
+    taco = tacos[index] # put the taco at the index in memory
+    puts "#{taco} tacos"
+    index = index + 1 # increment the index
+end
+
+## short way
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+for taco in tacos # taco is variable name created to each element in the array while writing this line
+    puts "#{taco} tacos"
+end
--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -9,6 +9,13 @@
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
+make pairs 
+for number in ranks
+    for suit in suits
+        puts "#{number} of #{suit}"
+    end
+end
+
 # Sample output:
 # 2 of Clubs
 # 2 of Diamonds
@@ -20,3 +27,13 @@ suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 # CHALLENGE
 # Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
 # You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
+
+# make pairs 
+cards = []
+for number in ranks
+    for suit in suits
+        card = "#{number} of #{suit}"
+        cards << card
+    end
+end
+puts cards.sample(5)
